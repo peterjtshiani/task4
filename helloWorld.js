79d5e98c20e9cbcecc9d485c224c64d4882cbf55
peter job tshiani <tshianipeter@gmail.com>
2019-08-23 08:56:56 +0200

    helloWorld

---
+++ helloWorld.js
@@ -0,0 +1,11 @@
+// creating a message that will display on the wedsite
+
+function helloWorld()
+	{
+		document.getElementById("message").innerHTML = "Git is Awesome!";
+		var name = window.prompt("Enter your name: ");
+		// alert("Your name is " + name);
+		document.getElementById("userName").innerHTML = "Your name is " + name
+
+	};
+	
